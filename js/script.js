52133d81dd38ed5090b8dcce52af7a8c247f2a32
abrar <abrarhameed911@gmail.com>
2024-11-16 02:27:25 +0500

    edit js

--- js/script.js
+++ js/script.js
@@ -311,7 +311,7 @@ document.querySelector(".range").getElementsByTagName("input")[0].addEventListen
 
 async function displayAlbums(folder0) {
     // Fetch the content of the folder
-    let response = await fetch(`${baseURL}/${folder}/`);
+    let response = await fetch(`${baseURL}/${folder0}/`);
 
     // Check if the fetch was successful
     if (!response.ok) {
