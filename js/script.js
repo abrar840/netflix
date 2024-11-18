ba9c255235fd7adb227bd30246c7390cd962d3f8
abrar <abrarhameed911@gmail.com>
2024-11-18 20:19:07 +0500

    removing error in js

--- js/script.js
+++ js/script.js
@@ -312,7 +312,7 @@ document.querySelector(".range").getElementsByTagName("input")[0].addEventListen
 async function displayAlbums(folder0) {
     // Fetch the content of the folder
     try {
-        let response = await fetch(`/${folder0}/`);
+        let response = await fetch(`${folder0}`);
         if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
     } catch (error) {
         console.error("Fetch failed:", error);
